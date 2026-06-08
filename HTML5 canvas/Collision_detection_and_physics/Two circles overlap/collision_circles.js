093764183e08fecb879fc99cbac7866e32f52509
Baotq1406 <quocbaotk1406@gmail.com>
2026-06-08 22:31:08 +0700

    Get the heading of the objects

--- HTML5 canvas/Collision_detection_and_physics/Two circles overlap/collision_circles.js
+++ HTML5 canvas/Collision_detection_and_physics/Two circles overlap/collision_circles.js
@@ -65,11 +65,24 @@ class Circle extends GameObject
         this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         this.context.fillStyle = this.isColliding ? "red" : "blue";
         this.context.fill();
+
+        // Draw heading vector
+        this.context.beginPath();
+        this.context.moveTo(this.x, this.y);
+        this.context.lineTo(this.x + this.vx, this.y + this.vy);
+        this.context.stroke();
     } 
 
     update(secondsPassed){
+        // Move with set velocity
         this.x += this.vx * secondsPassed;
         this.y += this.vy * secondsPassed;
+
+        // Calculate the angle (vy before vx)
+        let radians = Math.atan2(this.vy, this.vx);
+
+        // Convert to degrees
+        let degrees = 180 * radians / Math.PI;
     }
 }
 
